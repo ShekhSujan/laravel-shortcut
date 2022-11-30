fdf53af750d704c413a3840d283272bbb834f7df
Sujan S <43309024+sujan97825@users.noreply.github.com>
2022-11-30 10:19:51 +0600

    Update bangla-slug.php

--- bangla-slug.php
+++ bangla-slug.php
@@ -6,3 +6,8 @@ function CleanURL($string, $delimiter = '-')
     $string = strtolower(trim(preg_replace("/[\/_|+ -]+/", $delimiter, $string), $delimiter));
     return $string;
 }
+
+$slug = CleanURL(" সম্পন্ন !#$%$%^ হল বঙ্গবন্ধু&**( টানেলের //? প্রথম টিউবের )()(**@%$^&*( কাজ  ");
+echo $slug;
+
+// Output: সম্পন্ন হল বঙ্গবন্ধু টানেলের প্রথম টিউবের কাজ
